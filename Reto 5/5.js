943678b6b13784fa6700ffcd1733031bc464a25a
Ramón G. Gisbert <gisbert.rg@gmail.com>
2023-12-08 23:55:22 +0100

    Mejorar desafío 5

--- Reto 5/5.js
+++ Reto 5/5.js
@@ -1,3 +1,25 @@
+// Segundo intento
+function cyberReindeer(road, time) {
+    let roadB = road.replace('S', '.')
+    let prevRoad = road
+
+    return Array(time)
+        .fill()
+        .map((_, i) => {
+            const nextSanta = prevRoad.indexOf('S') + 1
+
+            if (i === 5) roadB = roadB.replaceAll('|', '*')
+            if (roadB[nextSanta] === '|' || i === 0) return prevRoad
+
+            prevRoad =
+                roadB.substring(0, nextSanta) +
+                'S' +
+                roadB.substring(nextSanta + 1)
+            return prevRoad
+        })
+}
+
+/* Primer intento
 function cyberReindeer(road, time) {
     const res = [road]
     let santaPos = road.indexOf('S')
@@ -18,3 +40,4 @@ function cyberReindeer(road, time) {
 
     return res
 }
+*/
